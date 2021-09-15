0ebe1e4dde04fb3286511a7ce4da66eadb622941
CherryVixxen <contato.mcsb@gmail.com>
2021-09-15 01:35:38 -0300

    upload Modulo 5 - cliente

---
+++ Desafios/Modulo5/cliente-back/README.md
@@ -0,0 +1,11 @@
+# Desafio - Modulo 5 - Cubos Academy
+
+### Links
+
+[Frontend do Restaurante "Restaurante-Front"](https://github.com/CherryVixxen/Cubos-Academy/tree/master/Desafios/Modulo5/dash-front)
+
+[Backend do Restaurante "Restaurante-Back"](https://github.com/CherryVixxen/Cubos-Academy/tree/master/Desafios/Modulo5/dash-back)
+
+[Frontend do Consumidor "Cliente-Front"](https://github.com/CherryVixxen/Cubos-Academy/tree/master/Desafios/Modulo5/cliente-front)
+
+[Backend do Consumidor "Cliente-Bak"](https://github.com/CherryVixxen/Cubos-Academy/tree/master/Desafios/Modulo5/cliente-back)
---
+++ Desafios/Modulo5/cliente-back/src/controladores/restaurantes.js
@@ -0,0 +1,50 @@
+const knex = require('../conexao');
+
+const listarRestaurantes = async (req, res) => {
+    const { busca } = req.query
+    const categoriaId = Number(req.query.categoriaId)
+    let restaurantes = '';
+
+    try {
+        const categorias = await knex('categorias');
+
+        if(categoriaId){
+            restaurantes = await knex('restaurantes')
+            .where('restaurantes.nome', 'ilike', `%${busca}%`)
+            .andWhere('restaurantes.categoria_id', '=', categoriaId);
+        } else{
+            restaurantes = await knex('restaurantes')
+            .where('restaurantes.nome', 'ilike', `%${busca}%`);
+        }
+
+        restaurantes.map(restaurante => {
+            categorias.find(categoria => {
+                if (categoria.id === restaurante.categoria_id) {
+                    restaurante.categoria = categoria;
+                }
+            })
+        })
+
+        return res.status(200).json(restaurantes);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+};
+
+const listarProdutosRestaurantes = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const produtos = await knex('produtos')
+            .where({ restaurante_id: id });
+
+        return res.status(200).json(produtos);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+};
+
+module.exports = {
+    listarRestaurantes,
+    listarProdutosRestaurantes
+}
